421e40584712d3650e177a33e2bd2734a3585aa0
Dariusz Burczyński <darekburczynski@protonmail.com>
2021-09-05 20:59:58 +0200

    Getter + Setter

--- src/devtaskutils/Tuple.java
+++ src/devtaskutils/Tuple.java
@@ -9,6 +9,22 @@ public class Tuple<X, Y> {
         this.y = y;
     }
 
+    public X getX() {
+        return x;
+    }
+
+    public void setX(X x) {
+        this.x = x;
+    }
+
+    public Y getY() {
+        return y;
+    }
+
+    public void setY(Y y) {
+        this.y = y;
+    }
+
     @Override
     public String toString() {
         return x.toString()+" "+y.toString();
