0c6af4f6db9098f3b6d9398eb458cc6bac37f1a4
Dariusz Burczyński <darekburczynski@protonmail.com>
2021-09-05 20:12:46 +0200

    Added finished DevTask1

---
+++ src/devtasks/DevTask1.java
@@ -0,0 +1,67 @@
+package devtasks;
+
+import devtaskutils.IntegerListCmdLineReader;
+import devtaskutils.DevTaskUtils;
+
+import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class DevTask1 {
+    public long getCount(List<Integer> integerList) {
+        return integerList == null ? 0L : integerList.stream().count();
+    }
+
+    public List<Integer> getDistinct(List<Integer> integerList) {
+        return integerList == null ? null : integerList.stream().distinct().collect(Collectors.toList());
+    }
+
+    public Integer getMin(List<Integer> integerList) {
+        try {
+            return integerList == null ? null : integerList.stream().mapToInt(i -> i).min().orElseThrow(NoSuchElementException:: new);
+        }
+        catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
+    public Integer getMax(List<Integer> integerList) {
+        try {
+            return integerList == null ? null : integerList.stream().mapToInt(i -> i).max().orElseThrow(NoSuchElementException:: new);
+        }
+        catch (NoSuchElementException ex) {
+            return null;
+        }
+    }
+
+    public List<Integer> sortAsc(List<Integer> integerList) {
+        return integerList == null ? null : integerList.stream().sorted().collect(Collectors.toList());
+    }
+
+    public String listToPrettyString(List<Integer> integerList) {
+        String readableString = "";
+        for (Integer integer : integerList) {
+            readableString += integer.toString() + " ";
+        }
+        return readableString;
+    }
+    public static void main(String[] args) {
+        System.out.println("Executing DevTask1...");
+        IntegerListCmdLineReader reader = new IntegerListCmdLineReader();
+        DevTask1 task1 = new DevTask1();
+        try {
+            List<Integer> integerList = reader.readIntegers();
+            List<Integer> distinctIntegerList = task1.getDistinct(integerList);
+
+            System.out.println(task1.listToPrettyString(distinctIntegerList));
+            System.out.println(String.format("count: %s", task1.getCount(integerList)));
+            System.out.println(String.format("distinct: %s", task1.getCount(distinctIntegerList)));
+            System.out.println(String.format("min: %s", task1.getMin(integerList)));
+            System.out.println(String.format("max: %s", task1.getMax(integerList)));
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
