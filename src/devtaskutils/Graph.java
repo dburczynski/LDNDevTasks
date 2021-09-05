698764e682e6b10f222b98f9b7ad686d5b01d48a
Dariusz Burczyński <darekburczynski@protonmail.com>
2021-09-05 23:01:14 +0200

    Added Graph and Vertex

---
+++ src/devtaskutils/Graph.java
@@ -0,0 +1,30 @@
+package devtaskutils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+public class Graph {
+
+    List<Vertex> vertexes;
+
+    public Graph() {
+        this.vertexes = new ArrayList<>();
+    }
+    public List<Vertex> getVertexes() {
+        return vertexes;
+    }
+
+    public void setVertexes(List<Vertex> vertexes) {
+        this.vertexes = vertexes;
+    }
+
+    public void addVertex(Vertex vertex) {
+        Optional<Vertex> vertexOptional = vertexes.stream()
+                .filter(v -> v.getVertex().equals(vertex.getVertex()))
+                .findFirst();
+
+        if(!vertexOptional.isPresent())
+            vertexes.add(vertex);
+    }
+}
---
+++ src/devtaskutils/Vertex.java
@@ -0,0 +1,39 @@
+package devtaskutils;
+
+import java.util.*;
+
+public class Vertex {
+
+    private Integer vertex;
+    private List<Integer> connections;
+
+    public Vertex(Integer vertex) {
+        this.vertex = vertex;
+        this.connections = new ArrayList<>();
+    }
+
+    public Integer getVertex() {
+        return vertex;
+    }
+
+    public void setVertex(Integer vertex) {
+        this.vertex = vertex;
+    }
+
+    public List<Integer> getConnections() {
+        return connections;
+    }
+
+    public void setConnections(List<Integer> connections) {
+        this.connections = connections;
+    }
+
+    public void addConnection(Integer connection) {
+      Optional<Integer> connectionOptional = connections.stream()
+              .filter(c -> c.equals(connection))
+              .findFirst();
+
+      if(!connectionOptional.isPresent())
+          connections.add(connection);
+    }
+}
